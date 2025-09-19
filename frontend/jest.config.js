b2b259fe4c98275ed463fdbea37d274fd56452af
Cherrou Mohamed <mohamedcherrou2000@gmail.com>
2025-09-19 22:56:48 +0100

    fix: separate Babel config for Jest to restore SWC build

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+    "^.+\\.(ts|tsx|js|jsx)$": [
+      "babel-jest",
+      { configFile: "./babel-jest.config.js" },
+    ],
   },
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
 };
