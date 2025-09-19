3ff8ef23810419b5650324cdc477a934e210b099
Cherrou Mohamed <mohamedcherrou2000@gmail.com>
2025-09-19 22:14:38 +0100

    chore: init backend + frontend

---
+++ backend/src/main/java/com/saasrh/backend/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.saasrh.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Backend up";
+    }
+}
